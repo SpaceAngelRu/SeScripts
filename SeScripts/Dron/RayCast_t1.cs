0371805b5ee33a24842c09020a691129fb466edb
Sergey Kotlyarov <gangelxxx@gmail.com>
2018-01-03 11:47:15 +0700

    +

---
+++ SeScripts/Dron/RayCast_t1.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Sandbox.Game.Entities;
+using Sandbox.Game.World;
+using Sandbox.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame;
+using VRageMath;
+
+namespace SeScripts.Dron
+{
+    public sealed class Program : MyGridProgram
+    {
+        #region Const
+
+        //Текстовая панель
+        private const string TEXT_PANEL = "Текстовая панель";
+        private const string CAM_NAME = "Зрительная камера";
+        private const string ROTOR_NAME = "Ротор камеры";
+
+        #endregion
+
+        #region Private fields
+
+        private IMyCameraBlock camera;
+
+        //    private IMyTextPanel lcd;
+        private bool firstrun = true;
+
+        private MyDetectedEntityInfo info;
+        private float PITCH = 0;
+        private StringBuilder sb = new StringBuilder();
+        private double SCAN_DISTANCE = 100;
+        private float YAW = 0;
+        private List<IMyCubeBlock> blocks = new List<IMyCubeBlock>();
+        private IMyMotorStator rotor;
+        #endregion
+
+        #region Construct
+
+        public Program()
+        {
+            PrintForAllTerminals("");
+
+            // Configure this program to run both Once and every 100 update ticks
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+
+            camera = GridTerminalSystem.GetBlockWithName(CAM_NAME) as IMyCameraBlock;
+            if (camera != null)
+                camera.EnableRaycast = true;
+            else
+            {
+                PrintForAllTerminals($"Не удалось найти камеру с именем {CAM_NAME}");
+            }
+
+            rotor = GridTerminalSystem.GetBlockWithName(ROTOR_NAME) as IMyMotorStator;
+            if (rotor == null)
+            {
+                PrintForAllTerminals($"Не удалось ротор с именем {ROTOR_NAME}");
+            }
+        }
+
+        #endregion
+
+        #region Public
+
+        public void Main(string argument)
+        {
+
+
+            //            if (rotor == null)
+            //            {
+            //                return;
+            //            }
+
+            //            MatrixD m = MatrixD.Identity;
+            //
+            var grid = this.Me.CubeGrid;
+            //
+            //            var dd = grid.GetCubeBlock()
+            //
+            //
+            var min = grid.Min;
+            var max = grid.Max;
+            //
+            var size = max - min + Vector3I.One;
+            var sizeInMeters = size * grid.GridSize;
+            //
+            if (camera.CanScan(SCAN_DISTANCE))
+                info = camera.Raycast(SCAN_DISTANCE, PITCH, YAW);
+
+            var dist = Vector3.Distance(Me.GetPosition(), info.Position);
+
+            sb.Clear();
+            sb.Append($"dist: {dist}");
+            sb.AppendLine();
+            sb.Append($"sizeInMeters: {sizeInMeters}");
+            sb.AppendLine();
+
+            if (rotor != null)
+            {
+                sb.Append($"angel: {rotor.Angle}");
+                sb.AppendLine();
+            }
+
+            //            Quaternion qu;
+            //            rotor.Orientation.GetQuaternion(out qu);
+            //
+            //            Vector3 axis;
+            //            float angle;
+            //            qu.GetAxisAngle(out axis, out angle);
+            //
+            //            sb.Append($"axis: {axis}");
+            //            sb.AppendLine();
+            //
+            //            sb.Append($"angle: {angle}");
+            //            sb.AppendLine();
+
+            //            sb.Append($"min: {grid.GridSize}");
+            //            sb.AppendLine();
+            //            sb.Append($"Grid size: {size}");
+            //            sb.AppendLine();
+            //            sb.Append($"Grid volume: {size.Volume()}");
+            //            sb.AppendLine();
+            //            sb.Append($"Size: {sizeInMeters} m^3");
+            //            sb.AppendLine();
+            //            sb.Append($"Volume: {sizeInMeters} m^3");
+            //            sb.AppendLine();
+            //            sb.Append($"blocks: {blocks.Count}");
+            //            sb.AppendLine();
+            //            sb.Append("EntityID: " + info.EntityId);
+            //            sb.AppendLine();
+            //            sb.Append("Name: " + info.Name);
+            //            sb.AppendLine();
+            //            sb.Append("Type: " + info.Type);
+            //            sb.AppendLine();
+            //            sb.Append("Velocity: " + info.Velocity.ToString("0.000"));
+            //            sb.AppendLine();
+            //            sb.Append("Relationship: " + info.Relationship);
+            //            sb.AppendLine();
+            //            sb.Append("Size: " + info.BoundingBox.Size.ToString("0.000"));
+            //            sb.AppendLine();
+            //            sb.Append("Position: " + info.Position.ToString("0.000"));
+
+            if (info.HitPosition.HasValue)
+            {
+                //                sb.AppendLine();
+                //                sb.Append("Hit: " + info.HitPosition.Value.ToString("0.000"));
+                //                sb.AppendLine(); 
+                //                sb.Append("Distance: " + Vector3D.Distance(camera.GetPosition(), info.HitPosition.Value).ToString("0.00"));
+            }
+
+            sb.AppendLine();
+            sb.Append("Range: " + camera.AvailableScanRange.ToString());
+
+            PrintForAllTerminals(sb.ToString());
+        }
+
+        //        private string GetDirectionString()
+        //        {
+        //            var cockpit = MySession.Static.ControlledEntity as MyCockpit;
+        //            if (cockpit != null)
+        //            {
+        //                Quaternion cockpitOrientation;
+        //                cockpit.Orientation.GetQuaternion(out cockpitOrientation);
+        //                var thrustDir = Vector3I.Transform(ThrustForwardVector, Quaternion.Inverse(cockpitOrientation));
+        //                if (thrustDir.X == 1)
+        //                    return MyTexts.GetString(MyCommonTexts.Thrust_Left);
+        //                else if (thrustDir.X == -1)
+        //                    return MyTexts.GetString(MyCommonTexts.Thrust_Right);
+        //                else if (thrustDir.Y == 1)
+        //                    return MyTexts.GetString(MyCommonTexts.Thrust_Down);
+        //                else if (thrustDir.Y == -1)
+        //                    return MyTexts.GetString(MyCommonTexts.Thrust_Up);
+        //                else if (thrustDir.Z == 1)
+        //                    return MyTexts.GetString(MyCommonTexts.Thrust_Forward);
+        //                else if (thrustDir.Z == -1)
+        //                    return MyTexts.GetString(MyCommonTexts.Thrust_Back);
+        //            }
+        //            return null;
+        //        }
+
+        #endregion
+
+        #region Private methods
+
+        private void PrintForAllTerminals(string str, bool append = false)
+        {
+            List<IMyTextPanel> textPanels = new List<IMyTextPanel>();
+            GridTerminalSystem.GetBlocksOfType(textPanels);
+
+            foreach (var textPanel in textPanels)
+            {
+                if (textPanel.CustomName.Contains(TEXT_PANEL))
+                {
+                    textPanel.WritePublicText(str, append);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ SeScripts/FillCargo/FillCargo.cs
@@ -0,0 +1,493 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sandbox.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame;
+
+#endregion
+
+namespace SeScripts.FillCargo
+{
+public sealed class Program : MyGridProgram
+{
+    #region Const
+
+    private const int POWER_CELL_AMOUNT = 100; //Аккумулятор
+    private const int BULLETPROOF_GLASS_AMOUNT = 100; //Бронированное стекло
+    private const int LARGE_STEEL_TUBE_AMOUNT = 1000; //Большая стальная труба
+    private const int GIRDER_AMOUNT = 100; //Балка
+    private const int THRUST_COMPONENTS_AMOUNT = 0; //Детали ускорителя
+    private const int COMPUTER_AMOUNT = 300; //Компьютер
+    private const int SMALL_STEEL_TUBE_AMOUNT = 1000; //Маленькая стальная трубка
+    private const int MEDICAL_COMPONENTS_AMOUNT = 100; //Медицинские компоненты
+    private const int DETECTOR_COMPONENTS_AMOUNT = 100; //Компоненты детектора
+    private const int GIR_GRAVITY_GENERATOR_COMPONENTS_DER_AMOUNT = 100; //Компоненты гравитационного генератора
+    private const int RADIO_COMMUNICATION_COMPONENTS_AMOUNT = 100; //Комплектующие для радио-связи
+    private const int REACTOR_COMPONENTS_AMOUNT = 1000; //Компоненты реактора
+    private const int MOTOR_AMOUNT = 1000; //Мотор
+    private const int INTERIOR_PLATE_AMOUNT = 1000; //Пластина
+    private const int METAL_GRID_AMOUNT = 3000; //Решетка
+    private const int STEEL_PLATE_AMOUNT = 3000; //Стальная пластина
+    private const int CONSTRUCTION_COMPONENT_AMOUNT = 1000; //Строительный компонент
+    private const int SOLAR_CELL_AMOUNT = 100; //Солнечная батарея
+    private const int DISPLAY_AMOUNT = 100; //Экран
+
+    //==================================================================================
+
+    #region Amount settings
+
+    //Сколько положить в целеовой контейнер
+    private const int MAX_AMOUT = 100;
+
+    #endregion
+
+    #endregion
+
+    #region Static
+
+    private static readonly StringBuilder MsgsBuffer = new StringBuilder();
+
+    #endregion
+
+    #region Private fields
+
+    private readonly string[] _signs = { "\\", "|", "/", "--" };
+    private int _curSign = 0;
+
+    private bool _isConnect = false;
+
+    #endregion
+
+    #region Construct
+
+    public Program()
+    { }
+
+    #endregion
+
+    #region Public
+
+    public void Main(string args)
+    {
+        var cargoContainers = new List<IMyTerminalBlock>();
+        GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(cargoContainers);
+
+        var cargoDestinations = GetCargoDist(cargoContainers, CARGO_DESTINATION);
+        if (cargoDestinations == null)
+        {
+            Print("Целевой контейнер не найден");
+            UpdatePrint();
+            return;
+        }
+
+        _isConnect = false;
+
+        var cargoSources = GetCargoSource(cargoContainers, CARGO_SOURCE);
+        if (cargoSources == null || cargoSources.Count == 0)
+        {
+            ShowCargoAmout(cargoDestinations);
+            Print("Контейнер базы не найден");
+            UpdatePrint();
+            return;
+        }
+
+        _isConnect = true;
+
+        //        ViewCargoInventory(cargoSources[0] as IMyCargoContainer);
+
+        try
+        {
+            TransferCargo(cargoSources, cargoDestinations, STEEL_PLATE, MAX_AMOUT, STEEL_PLATE_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, COMPUTER, MAX_AMOUT, COMPUTER_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, DISPLAY, MAX_AMOUT, DISPLAY_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, CONSTRUCTION_COMPONENT, MAX_AMOUT, CONSTRUCTION_COMPONENT_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, INTERIOR_PLATE, MAX_AMOUT, INTERIOR_PLATE_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, THRUST_COMPONENTS, MAX_AMOUT, THRUST_COMPONENTS_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, BULLETPROOF_GLASS, MAX_AMOUT, BULLETPROOF_GLASS_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, DETECTOR_COMPONENTS, MAX_AMOUT, DETECTOR_COMPONENTS_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, GIRDER, MAX_AMOUT, GIRDER_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, GIR_GRAVITY_GENERATOR_COMPONENTS_DER, MAX_AMOUT,
+                GIR_GRAVITY_GENERATOR_COMPONENTS_DER_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, LARGE_STEEL_TUBE, MAX_AMOUT, LARGE_STEEL_TUBE_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, MEDICAL_COMPONENTS, MAX_AMOUT, MEDICAL_COMPONENTS_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, METAL_GRID, MAX_AMOUT, METAL_GRID_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, MOTOR, MAX_AMOUT, MOTOR_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, POWER_CELL, MAX_AMOUT, POWER_CELL_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, RADIO_COMMUNICATION_COMPONENTS, MAX_AMOUT, RADIO_COMMUNICATION_COMPONENTS_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, SMALL_STEEL_TUBE, MAX_AMOUT, SMALL_STEEL_TUBE_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, REACTOR_COMPONENTS, MAX_AMOUT, REACTOR_COMPONENTS_AMOUNT);
+            TransferCargo(cargoSources, cargoDestinations, SOLAR_CELL, MAX_AMOUT, SOLAR_CELL_AMOUNT);
+
+            ShowCargoAmout(cargoDestinations);
+        }
+        catch (Exception e)
+        {
+            Print(e.Message);
+        }
+
+        UpdatePrint();
+    }
+
+    public void Save()
+    { }
+
+    #endregion
+
+    #region Private methods
+
+    private static int GetIdxItems(List<IMyInventoryItem> inventoryItems, string subtypeName)
+    {
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].Content.SubtypeName == subtypeName)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private void TransferCargo(List<IMyTerminalBlock> cargoSources, List<IMyTerminalBlock> cargoDestination, string subtypeName,
+        int transferAmount,
+        int maxAmout)
+    {
+        if (cargoDestination == null || cargoDestination.Count == 0)
+        {
+            Print($"Контейнеры для материаллов не найден ");
+            return;
+        }
+
+        if (cargoSources == null || cargoSources.Count == 0)
+        {
+            Print("Контейнеры с материалами не подключен");
+            return;
+        }
+
+        foreach (var src in cargoSources)
+        {
+            IMyInventory inventorySrc = src.GetInventory();
+
+            var invontorySrc = inventorySrc.GetItems();
+            int idxSrc = GetIdxItems(invontorySrc, subtypeName);
+
+            if (idxSrc < 0)
+            {
+                continue;
+            }
+
+            foreach (var dst in cargoDestination)
+            {
+                IMyInventory inventoryDest = dst.GetInventory();
+
+                if (inventoryDest.IsFull)
+                {
+                    continue;
+                }
+
+                var invontoryDests = inventoryDest.GetItems();
+                int idxDst = GetIdxItems(invontoryDests, subtypeName);
+
+                if (idxDst < 0)
+                {
+                    if (maxAmout > 0)
+                        inventorySrc.TransferItemTo(inventoryDest, idxSrc, null, null, transferAmount);
+                }
+                else
+                {
+                    int a = maxAmout - invontoryDests[idxDst].Amount.ToIntSafe();
+                    if (a > 0)
+                    {
+                        inventorySrc.TransferItemTo(inventoryDest, idxSrc, null, null, a);
+                    }
+                    else
+                    {
+                        inventoryDest.TransferItemTo(inventorySrc, idxDst, null, null, Math.Abs(a));
+                    }
+                }
+            }
+        }
+    }
+
+    private void PrintForAllTerminals(string str, bool append = false)
+    {
+        List<IMyTextPanel> textPanels = new List<IMyTextPanel>();
+        GridTerminalSystem.GetBlocksOfType(textPanels);
+
+        foreach (var textPanel in textPanels)
+        {
+            if (textPanel.CustomName.Contains(TEXT_PANEL))
+            {
+                textPanel.WritePublicText(str, append);
+            }
+        }
+    }
+
+    private void UpdatePrint()
+    {
+        _curSign = _curSign < 3 ? ++_curSign : 0;
+
+        if (_isConnect)
+            PrintForAllTerminals("cc " + DateTime.Now.ToString("T") + "  " + _signs[_curSign] + "\n");
+        else
+            PrintForAllTerminals(DateTime.Now.ToString("T") + "  " + _signs[_curSign] + "\n");
+
+        if (MsgsBuffer.Length > 0)
+        {
+            PrintForAllTerminals(MsgsBuffer.ToString(), true);
+        }
+
+        if (MsgsBuffer.Length > MAX_SYMBOLS)
+        {
+            MsgsBuffer.Clear();
+        }
+    }
+
+    private void Print(string str)
+    {
+        MsgsBuffer.AppendLine(str);
+    }
+
+    private List<IMyTerminalBlock> GetCargoDist(List<IMyTerminalBlock> listCargo, string name)
+    {
+        return listCargo.Where(cargo => cargo.CustomName.Contains(name)).ToList();
+    }
+
+    private List<IMyTerminalBlock> GetCargoSource(List<IMyTerminalBlock> listCargo, string name)
+    {
+        return listCargo.Where(item => item.CustomName.Contains(name)).ToList();
+    }
+
+    private void ViewCargoInventory(IMyCargoContainer cargo)
+    {
+        if (cargo == null)
+        {
+            Print("Контейнер не найден");
+            return;
+        }
+
+        Print($"Контейнер: {cargo.CustomName}");
+        IMyInventory inventory = cargo.GetInventory();
+
+        if (inventory != null)
+        {
+            Print($"items: {inventory.GetItems().Count}");
+            foreach (var item in inventory.GetItems())
+            {
+                Print(
+                    $"{cargo.CustomName} \n\t\tN: {item.Content.SubtypeName}, \n\t\tI: {item.Content.SubtypeId}, \n\t\tT: {item.Content.TypeId}");
+            }
+        }
+        else
+        {
+            Print("Конейнер пуст");
+        }
+    }
+
+    private void ShowCargoAmout(List<IMyTerminalBlock> cargoList)
+    {
+        MsgsBuffer.Clear();
+
+        if (cargoList == null)
+        {
+            Print("Контейнер не найден");
+            return;
+        }
+
+        int steelPlate = 0;
+        int thrustComponents = 0;
+        int bulletproofGlass = 0;
+        int computer = 0;
+        int constructionComponent = 0;
+        int detectorComponents = 0;
+        int display = 0;
+        int girder = 0;
+        int girGravityGeneratorComponentsDer = 0;
+        int interiorPlate = 0;
+        int largeSteelTube = 0;
+        int medicalComponents = 0;
+        int metalGrid = 0;
+        int motor = 0;
+        int powerCell = 0;
+        int radioCommunicationComponents = 0;
+        int reactorComponents = 0;
+        int smallSteelTube = 0;
+        int solarCell = 0;
+
+        foreach (var cargo in cargoList)
+        {
+            IMyInventory inventory = cargo.GetInventory();
+            if (inventory != null)
+            {
+                foreach (var item in inventory.GetItems())
+                {
+                    switch (item.Content.SubtypeName)
+                    {
+                        case STEEL_PLATE:
+                            steelPlate = item.Amount.ToIntSafe();
+                            break;
+                        case THRUST_COMPONENTS:
+                            thrustComponents = item.Amount.ToIntSafe();
+                            break;
+                        case BULLETPROOF_GLASS:
+                            bulletproofGlass = item.Amount.ToIntSafe();
+                            break;
+                        case COMPUTER:
+                            computer = item.Amount.ToIntSafe();
+                            break;
+                        case CONSTRUCTION_COMPONENT:
+                            constructionComponent = item.Amount.ToIntSafe();
+                            break;
+                        case DETECTOR_COMPONENTS:
+                            detectorComponents = item.Amount.ToIntSafe();
+                            break;
+                        case DISPLAY:
+                            display = item.Amount.ToIntSafe();
+                            break;
+                        case GIRDER:
+                            girder = item.Amount.ToIntSafe();
+                            break;
+                        case GIR_GRAVITY_GENERATOR_COMPONENTS_DER:
+                            girGravityGeneratorComponentsDer = item.Amount.ToIntSafe();
+                            break;
+                        case INTERIOR_PLATE:
+                            interiorPlate = item.Amount.ToIntSafe();
+                            break;
+                        case LARGE_STEEL_TUBE:
+                            largeSteelTube = item.Amount.ToIntSafe();
+                            break;
+                        case MEDICAL_COMPONENTS:
+                            medicalComponents = item.Amount.ToIntSafe();
+                            break;
+                        case METAL_GRID:
+                            metalGrid = item.Amount.ToIntSafe();
+                            break;
+                        case MOTOR:
+                            motor = item.Amount.ToIntSafe();
+                            break;
+                        case POWER_CELL:
+                            powerCell = item.Amount.ToIntSafe();
+                            break;
+                        case RADIO_COMMUNICATION_COMPONENTS:
+                            radioCommunicationComponents = item.Amount.ToIntSafe();
+                            break;
+                        case REACTOR_COMPONENTS:
+                            reactorComponents = item.Amount.ToIntSafe();
+                            break;
+                        case SMALL_STEEL_TUBE:
+                            smallSteelTube = item.Amount.ToIntSafe();
+                            break;
+                        case SOLAR_CELL:
+                            solarCell = item.Amount.ToIntSafe();
+                            break;
+                    }
+                }
+            }
+        }
+
+        Print($"Стальная пластина             {steelPlate}");
+        Print($"Детали ускорителя             {thrustComponents}");
+        Print($"Бронированное стекло          {bulletproofGlass}");
+        Print($"Компьютер                     {computer}");
+        Print($"Строительный компонент        {constructionComponent}");
+        Print($"Компоненты детектора          {detectorComponents}");
+        Print($"Экран                         {display}");
+        Print($"Балка                         {girder}");
+        Print($"Гравитационного генератора    {girGravityGeneratorComponentsDer}");
+        Print($"Пластина                      {interiorPlate}");
+        Print($"Большая стальная труба        {largeSteelTube}");
+        Print($"Медицинские компоненты        {medicalComponents}");
+        Print($"Металлическая решетка         {metalGrid}");
+        Print($"Мотор                         {motor}");
+        Print($"Аккумулятор                   {powerCell}");
+        Print($"Комплектующие для радио-связи {radioCommunicationComponents}");
+        Print($"Компоненты реактора           {reactorComponents}");
+        Print($"Маленькая стальная трубка     {smallSteelTube}");
+        Print($"Солнечная батарея             {solarCell}");
+    }
+
+    #endregion
+
+    #region Settings
+
+    //Максимальное колличество символов которое возможно вывести без очистки буфера сообщений
+    private const int MAX_SYMBOLS = 200;
+
+    //Часть имени целевого контейнера
+    private const string CARGO_DESTINATION = "Контейнер сборщика";
+
+    //Часть имени исходного контейнера
+    private const string CARGO_SOURCE = "Контейнер с материаллами базы";
+
+    //Текстовая панель
+    private const string TEXT_PANEL = "Текстовая панель";
+
+    #endregion 
+
+    #region SubtypeName
+
+    //Стальная пластина
+    private const string STEEL_PLATE = "SteelPlate";
+
+    //Детали ускорителя
+    private const string THRUST_COMPONENTS = "Thrust";
+
+    //Бронированное стекло
+    private const string BULLETPROOF_GLASS = "BulletproofGlass";
+
+    //Компьютер
+    private const string COMPUTER = "Computer";
+
+    //Строительный компонент
+    private const string CONSTRUCTION_COMPONENT = "Construction";
+
+    //Компоненты детектора
+    private const string DETECTOR_COMPONENTS = "Detector";
+
+    //Экран
+    private const string DISPLAY = "Display";
+
+    //Балка
+    private const string GIRDER = "Girder";
+
+    //Компоненты гравитационного генератора 
+    private const string GIR_GRAVITY_GENERATOR_COMPONENTS_DER = "GravityGenerator";
+
+    //Пластина
+    private const string INTERIOR_PLATE = "InteriorPlate";
+
+    //Большая стальная труба
+    private const string LARGE_STEEL_TUBE = "LargeTube";
+
+    //Медицинские компоненты
+    private const string MEDICAL_COMPONENTS = "Medical";
+
+    //Металлическая решетка
+    private const string METAL_GRID = "MetalGrid";
+
+    //Мотор
+    private const string MOTOR = "Motor";
+
+    //Аккумулятор
+    private const string POWER_CELL = "PowerCell";
+
+    //Комплектующие для радио-связи
+    private const string RADIO_COMMUNICATION_COMPONENTS = "RadioCommunication";
+
+    //Компоненты реактора
+    private const string REACTOR_COMPONENTS = "Reactor";
+
+    //Маленькая стальная трубка
+    private const string SMALL_STEEL_TUBE = "SmallTube";
+
+    //Солнечная батарея
+    private const string SOLAR_CELL = "SolarCell";
+
+    #endregion
+
+    //==================================================================================
+}
+}
